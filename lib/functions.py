961bb699aeced46b69b27301dd7a4b70ecaf4cc8
Error7799 <kingy7799sentamu@gmail.com>
2024-09-01 10:41:48 +0300

    python is so cool

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
-def greet_with_default(name="programmer"):
-    pass
+def greet_with_default(name = "programmer"):
+    print(f"Hello, {name}!")
 
-def add(num1, num2):
-    pass
+def add(num1,num2):
+    return num1 + num2
 
 def halve(number):
-    pass
+    if not isinstance(number,(float, int)):
+        return None
+    return number / 2
